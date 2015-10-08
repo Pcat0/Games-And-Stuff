e3813248cc95ff46e7e9f7d91e1b8a581cec6b3e
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-08 08:44:29 -0500

    thing

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -82,13 +82,13 @@ var TicTacToe = function() {
     if (this.get(1,3) + this.get(2,2) + this.get(3,1) == -3) {return "-1";}
    return 0;
   }
-  
-  return this;
-};
-this.AI = function(miss){
-  this.test = function() {
-    console.log("vjhvjvjhvh")
+  this.AI = function(miss){
+    this.test = function() {
+      console.log("vjhvjvjhvh")
+    }
+    return this;
   }
   return this;
-}
+};
+
 var tictactoe = new TicTacToe();
