ccc5dccaf668cbb4a894e32f76afc5df2e43c571
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-09 13:49:28 -0500

    add more things

--- snake/snake.js
+++ snake/snake.js
@@ -1,1 +1,12 @@
-
+var Snake = function() {
+    this.snakeBody = [];
+    this.moveArray = function(array) {
+        var i = array.length;
+        while (i <= 0) {
+            i = i - 1;
+            array[i + 1] = array[i];
+        }
+        return i;
+    };
+};
+var Snake = new Snake();
