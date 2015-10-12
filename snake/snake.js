4175e6f923f54d4627a3cb8a3beeee1305fb567d
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-12 11:23:40 -0500

    Update snake.js

--- snake/snake.js
+++ snake/snake.js
@@ -1,12 +1,14 @@
 var Snake = function() {
     this.snakeBody = [];
-    this.iterateArray = function(array) {
-        var i = array.length;
-        while (i >= 0) {
-            i = i - 1;
-            array[i + 1] = array[i];
-        }
-        return array;
+    this.direction = 1;
+    this.facing = 1;
+    var map = [];
+    onkeydown = onkeyup = function(e){
+        e = e || event; // to deal with IE
+        //map[e.keyCode] = e.type == 'keydown';
+        alert(e.keyCode);
+        
+        
     };
 };
 var snake = new Snake();
