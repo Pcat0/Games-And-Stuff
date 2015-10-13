87e15994485fea6d6fd3366edd59571fcc421cf1
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-13 08:37:01 -0500

    add move

--- snake/snake.js
+++ snake/snake.js
@@ -1,14 +1,19 @@
 var Snake = function() {
-    this.snakeBody = [];
-    this.direction = 1;
-    this.facing = 1;
-    var map = [];
+    var c = document.getElementById("gameBoard");
+    var ctx = c.getContext("2d");
+    this.snakeBody = [[5,5]];
+    this.direction = 1; //1 = right/down  -1 = left/up
+    this.facing = 1; //1 = left/right 2 = up/down  
     onkeydown = onkeyup = function(e){
         e = e || event; // to deal with IE
-        //map[e.keyCode] = e.type == 'keydown';
+        map[e.keyCode] = e.type == 'keydown';
         alert(e.keyCode);
         
         
     };
+    this.move(){
+        this.snakeBody.unshift(this.snakeBody[0[this.facing]]+this.direction);
+    }
+    return this;
 };
 var snake = new Snake();
