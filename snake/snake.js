4e281f3e171fec1037c297db6119974c9cac6393
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-13 10:29:23 -0500

    fix names and stuff

--- snake/snake.js
+++ snake/snake.js
@@ -5,13 +5,14 @@ var Snake = function() {
     this.snakeBody = [[5,5]];//x,y
     this.direction = 0;//0=up 1=right 2=down 3=left
     onkeydown = onkeyup = function(e){
+        var map = 0;
         e = e || event; // to deal with IE
         map[e.keyCode] = e.type == 'keydown';
         alert(e.keyCode);
     };
     this.move = function(){
         var newBody = this.snakeBody[0];
-        switch (direction) {
+        switch (snake.direction) {
             case 0:
                 newBody[1] = newBody[1]-1;
                 break;
