de6b788a19a3d1a928700f8f96afcb5b0379195b
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-20 09:38:56 -0500

    add board/die

--- snake/snake.js
+++ snake/snake.js
@@ -1,8 +1,7 @@
 var c = document.getElementById("gameBoard");
 var ctx = c.getContext("2d");
-
+var board = [];
 var Snake = function() {
-    
     this.snakeBody = [[5,5]];//x,y
     this.direction = 1;//0=up 1=right 2=down 3=left
     this.length = 5;
@@ -29,14 +28,19 @@ var Snake = function() {
         this.snakeBody.unshift(newBody);
         if (this.snakeBody.length > this.length){
             ctx.clearRect((this.snakeBody[this.snakeBody.length - 1][0] * 20),(this.snakeBody[this.snakeBody.length - 1][1] * 20),20,20);
-            this.snakeBody.pop()
+            board[(this.snakeBody[this.snakeBody.length - 1][0]][this.snakeBody[this.snakeBody.length - 1][1]] = 0;
+            this.snakeBody.pop();
         }
+        if (board[(newBody[0]][newBody[1]] == 1) {
+            console.log("die")
+        }
+        board[(newBody[0]][newBody[1]] = 1;
     };
     this.start = function() {
-        main = setInterval(function(){snake.move()},120);
+        return main = setInterval(function(){snake.move()},120);
     };
     this.stop = function() {
-        clearInterval(main);
+        return clearInterval(main);
     };
     return this;
 };
@@ -44,31 +48,29 @@ var Food = function() {
     this.coordinate = [Math.floor((Math.random() * 50) + 1), Math.floor((Math.random() * 50) + 1)];
     ctx.fillStyle = "#FFFF00";
     ctx.fillRect(this.coordinate[0] * 20,this.coordinate[1] * 20,20,20);
-}
+};
 var food = new Food();
 var snake = new Snake();
 onkeydown = onkeyup = function(e){
-        var map = [];
-        e = e || event; // to deal with IE
-        map[e.keyCode] = e.type == 'keydown';
-        console.log(map);
-        if (map[38]) {
-            console.log("1");
-            snake.direction = 0;
-        }
-        if (map[39]) {
-            console.log("2");
-            snake.direction = 1;
-        }
-        if (map[40]) {
-            console.log("3");
-            snake.direction = 2;
-        }
-        if (map[37]) {
-            console.log("4");
-            snake.direction = 3;
-        }
-        
-    };
-    //
+    var map = [];
+    e = e || event; // to deal with IE
+    map[e.keyCode] = e.type == 'keydown';
+    console.log(map);
+    if (map[38]) {
+        console.log("1");
+        snake.direction = 0;
+    }
+    if (map[39]) {
+        console.log("2");
+        snake.direction = 1;
+    }
+    if (map[40]) {
+        console.log("3");
+        snake.direction = 2;
+    }
+    if (map[37]) {
+        console.log("4");
+        snake.direction = 3;
+    }
+};
 
