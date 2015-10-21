28442ebca99dc866a49da309b15c65c5cb6c4055
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-21 13:27:37 -0500

    i

--- snake/snake.js
+++ snake/snake.js
@@ -1,6 +1,7 @@
 var c = document.getElementById("gameBoard");
 var ctx = c.getContext("2d");
 var board = [];
+var i = 1;
 while (i <= 50) {board[i] = []; i++;}
 var Snake = function() {
     this.snakeBody = [[5,5]];//x,y
