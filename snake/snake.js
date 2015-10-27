7cce63d3ea81337e7e048e03f98bb95538d7db9e
Patrick Ambli <patrick.ambli@gmail.com>
2015-10-27 09:12:42 -0500

    bug fix

--- snake/snake.js
+++ snake/snake.js
@@ -53,7 +53,7 @@ var Snake = function(Xstart, Ystart, color) {
     };
     return this;
 };
-gameStop = function(loser){
+var gameStop = function(loser){
     if (playerNum == 2) {
         stop(snake);
         stop(snake2);
@@ -64,19 +64,19 @@ gameStop = function(loser){
         }
     }
 };
-Food = function() {
+var Food = function() {
     this.coordinate = [Math.floor((Math.random() * (sizeX/20)) + 1), Math.floor((Math.random() * (sizeY/20)) + 1)];
     ctx.fillStyle = "#FFFF00";
     ctx.fillRect(this.coordinate[0] * 20,this.coordinate[1] * 20,20,20);
     board[this.coordinate[0]][this.coordinate[1]] = 2;
 };
-start = function(snakeName) {
+var start = function(snakeName) {
    return snakeName.main = setInterval(function(){snakeName.move()},120);
 };
-stop = function(snakeName) {
+var stop = function(snakeName) {
     return clearInterval(snakeName.main);
 };
-reset = function() {
+var reset = function() {
     snake = null;
     snake2 = null;
     board = [];
@@ -89,7 +89,7 @@ reset = function() {
         snake2 = new Snake(1, (sizeY/ 20 - 1), '#'+Math.floor(Math.random()*16777215).toString(16));
     }
 }
-var onkeydown = onkeyup = function(e){
+onkeydown = onkeyup = function(e){
     var map = [];
     e = e || event; // to deal with IE
     map[e.keyCode] = e.type == 'keydown';
