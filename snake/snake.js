be1336b50281b601739a81cea6ee16861cf56997
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-01 13:38:33 -0600

    test

--- snake/snake.js
+++ snake/snake.js
@@ -137,10 +137,10 @@ var wsOpen = function(){
 		console.log('Received:'+ st_received_message);
 		var received_message = JSON.parse(st_received_message);
 		//console.log(received_message.message.direction);
-		snake.direction = received_message.message.direction;
 		if (received_message.message.start) {
 			gameStart(1);
 		}
+		snake.direction = received_message.message.direction;
 	};
 	// add event handler for diconnection 
 	ws.onclose = function(evt){
