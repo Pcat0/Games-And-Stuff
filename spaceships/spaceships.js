352c162a90ae56dff53b093b1ea6b9ba4964342a
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-02 13:19:55 -0600

    test

--- spaceships/spaceships.js
+++ spaceships/spaceships.js
@@ -2,7 +2,7 @@ var spaceShip = document.createElement('span');
 spaceShip.style.backGround = 'blue';
 spaceShip.style.hight = '10px';
 spaceShip.style.width = '10px';
-document.body.appendChild(spaceShip);
+document.body.appendChild('spaceShip');
 var Objects = function() {
   this.draw = function() {
     
