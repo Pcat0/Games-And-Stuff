aa1d0794593328193178375cbf685821db2fd885
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-03 08:35:12 -0600

    test DOM

--- spaceships/spaceships.js
+++ spaceships/spaceships.js
@@ -1,11 +1,16 @@
-var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "white";
-div.innerHTML = "Hello";
-document.body.appendChild(div);
+var span = document.createElement("span");
+span.style.width = "100px";
+span.style.height = "100px";
+span.style.background = "red";
+span.style.color = "white";
+span.innerHTML = "Hello";
+document.body.appendChild(span);
 var Objects = function() {
+  this.data = {
+    'icon': null
+    'sizeX': null
+    'sizeY': null
+  }
   this.draw = function() {
     
   };
