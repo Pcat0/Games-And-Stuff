353ed78c90e309fb6b42c9b816d1bb375a19a2c3
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-03 11:08:07 -0600

    remove test box

--- spaceships/spaceships.js
+++ spaceships/spaceships.js
@@ -1,10 +1,3 @@
-var div = document.createElement("div");
-div.style.width = "100px";
-div.style.height = "100px";
-div.style.background = "red";
-div.style.color = "white";
-div.innerHTML = "Hello";
-document.body.appendChild(div);
 var Objects = function() {
   this.data = {
     'icon': null,
