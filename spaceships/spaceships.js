7fa674cd90e12fcbcb1d10cd1a09fca5a586bf3a
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-03 08:57:10 -0600

    DOM test

--- spaceships/spaceships.js
+++ spaceships/spaceships.js
@@ -1,17 +1,24 @@
-var span = document.createElement("span");
-span.style.width = "100px";
-span.style.height = "100px";
-span.style.background = "red";
-span.style.color = "white";
-span.innerHTML = "Hello";
-document.body.appendChild(span);
+var div = document.createElement("div");
+div.style.width = "100px";
+div.style.height = "100px";
+div.style.background = "red";
+div.style.color = "white";
+div.innerHTML = "Hello";
+document.body.appendChild(div);
 var Objects = function() {
   this.data = {
-    'icon': null,
+    'icon': 'http://www.planwallpaper.com/static/images/Winter-Tiger-Wild-Cat-Images.jpg',
     'sizeX': null,
     'sizeY': null
   }
   this.draw = function() {
-    
+    this.self = document.createElement("div");
+    this.self.style.width = this.data.sizeX;
+    this.self.style.height = this.data.sizeY;
+    this.self.innerHTML = "Hello";
+    document.body.appendChild(this.self);
+    var this.img = document.createElement("img");
+    this.img.src = this.data.icon;
+    this.img.appendChild(DOM_img);
   };
 };
