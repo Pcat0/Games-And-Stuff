3b4f041dbf019d0cf5ac466b25f2e608ce25a283
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-06 14:15:03 -0600

    bug fix

--- spaceships/spaceships.js
+++ spaceships/spaceships.js
@@ -79,8 +79,8 @@ function wsOpen(){
           ws.send(JSON.stringify({'toS': received_message.sID, 'mess':{'join': true,'x': ships[sId].x,'y': ships[sId].y, 'r': ships[sId].r, 'vx':ships[sId].vx, 'vy':ships[sId].vy, 'newS':false}}));
         }
       }
-      if (received_message.mess.xv != undefined && received_message.mess.join == undefined) {
-        ships[received_message.sID].xv = received_message.mess.xy; ships[received_message.sID].yv = received_message.mess.yv;
+      if (received_message.mess.vx != undefined && received_message.mess.join == undefined) {
+        ships[received_message.sID].vx = received_message.mess.vx; ships[received_message.sID].vy = received_message.mess.vy;
       }
       if (received_message.mess.r != undefined && received_message.mess.join == undefined) {
         ships[received_message.sID].rotate(received_message.mess.r);
