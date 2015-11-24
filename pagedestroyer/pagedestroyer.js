a56f34d767d70cd1832434720dc2e933edcc556b
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-24 15:18:07 -0600

    Create pagedestroyer.js

---
+++ pagedestroyer/pagedestroyer.js
@@ -0,0 +1,29 @@
+var x = 0;
+var y = 0;
+var height = screen.height;
+var width = screen.width;
+var blockX = Math.ceil(width / 50);
+var blockY = Math.ceil(height / 50);
+document.body.style.margin = '0px';
+html2canvas(document.body, {
+  onrendered: function(canvas) {
+    document.body.innerHTML = "";
+    while (y < height) {
+      while (x < width){
+        ctx = canvas.getContext("2d");
+        var squ = document.createElement('canvas');
+        squ.width = blockX;
+        squ.height = blockY;
+        squ.style.top = y + 'px';
+        squ.style.left = x + 'px';
+        squ.style.position = 'absolute';
+        squ.getContext("2d").putImageData(ctx.getImageData(x,y,x+blockX,y+blockY), 0, 0);
+        document.body.appendChild(squ);
+        x += blockX;
+      }
+      x = 0;
+      y += blockY;
+    }
+  }
+});
+//<script src='https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js'></script>
