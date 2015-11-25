5b8624fad8c80da430b7ae67aa62da1a017d2b25
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-25 10:56:56 -0600

    test

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -5,10 +5,13 @@ var width = screen.width;
 var blockX = Math.ceil(width / 50);
 var blockY = Math.ceil(height / 50);
 document.body.style.margin = '0px';
+var items = [];
+
 var fileref=document.createElement('script');
 fileref.setAttribute("type","text/javascript");
 fileref.setAttribute("src", 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js');
-document.body.appendChild(fileref)
+document.body.appendChild(fileref);
+
 html2canvas(document.body, {onrendered: function(canvas) {
   document.body.innerHTML = "";
   document.body.style.backgroundColor = 'lightgrey';
@@ -29,6 +32,7 @@ html2canvas(document.body, {onrendered: function(canvas) {
       y += blockY;
       var boxSize = 30;
       var all;
+      setInterval(function(){ships.forEach(function(a){a.tick()})}, 1);
       onclick = function(e) {
         var i = 0;
         var all = document.getElementsByTagName("canvas");
@@ -38,7 +42,9 @@ html2canvas(document.body, {onrendered: function(canvas) {
           var box = all[i].getBoundingClientRect();
           if (box.left < (e.x + boxSize) && (box.left + box.width) > (e.x - boxSize) && box.top < (e.y + boxSize) && (box.top + box.height) > (e.y - boxSize)) {
               //document.body.removeChild(all[i]);
-              i -= 1;
+              var _i = items.push(new move(all[i])) - 1;
+              item[_i].vSet(2);
+              //i -= 1;
             }
           }
         }
