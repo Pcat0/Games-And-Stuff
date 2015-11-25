478bfa810fc2e2fd8ffd0e0618331092b4112e20
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-25 15:11:17 -0600

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -10,8 +10,9 @@ var tool = 'hammer';
 var gravity = .5;
 var s=document.createElement('script');s.setAttribute("type","text/javascript");s.setAttribute("src", 'https://Pcat0.github.io/utilities/scriptLoader.js');document.body.appendChild(s);
 s.onload = function(){
-LOADJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js', true, onLoad());
-LOADJS('keyCodes');
+  LOADJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js', true, onLoad());
+  LOADJS('keyCodes');
+}
 var onLoad = function(){
 html2canvas(document.body, {onrendered: function(canvas) {
   document.body.innerHTML = "";
