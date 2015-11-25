c19c736e0fb5cbc3417ea9acb0499550a0f888a3
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-25 15:21:55 -0600

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -8,12 +8,15 @@ document.body.style.margin = '0px';
 var items = [];
 var tool = 'hammer';
 var gravity = .5;
+/*
 var s=document.createElement('script');s.setAttribute("type","text/javascript");s.setAttribute("src", 'https://Pcat0.github.io/utilities/scriptLoader.js');document.body.appendChild(s);
 s.onload = function(){
   LOADJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js', true, onLoad());
   LOADJS('keyCodes');
 }
-var onLoad = function(){
+*/
+var s=document.createElement('script');s.setAttribute("type","text/javascript");s.setAttribute("src", 'https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js');document.body.appendChild(s);
+s.onLoad = function(){
 html2canvas(document.body, {onrendered: function(canvas) {
   document.body.innerHTML = "";
   document.body.style.backgroundColor = 'lightgrey';
