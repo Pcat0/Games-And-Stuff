7091ab93903a6285c4b890f08a51471a5b9e4314
Patrick Ambli <patrick.ambli@gmail.com>
2015-11-25 14:46:08 -0600

    Update scriptLoader.js

--- utilities/scriptLoader.js
+++ utilities/scriptLoader.js
@@ -1,1 +1,6 @@
-function(){
+var LOADJS =  function(des, out){
+  des = (out) ? des : 'https://Pcat0.github.io/utilities/' + des;
+  des = (des.includes('https://')) ? des : 'https://' + des;
+  des = (des.endsWith('.js')) ? des : des + ".js";
+  var s=document.createElement('script');s.setAttribute("type","text/javascript");s.setAttribute("src", des);document.body.appendChild(s);
+}
