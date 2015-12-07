ec07be566e6b2edb0fd94a8360d0b201a8e7d4aa
Patrick Ambli <patrick.ambli@gmail.com>
2015-12-07 08:53:13 -0600

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -122,3 +122,9 @@ var move = function(item) {
     if (this.y > height){this.y = height; this.vy = 0;}
   };
 };
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-69409530-1', 'auto');
+ga('send', 'pageview');
