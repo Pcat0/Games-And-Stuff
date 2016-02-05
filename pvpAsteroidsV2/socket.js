606dc420ea484283803ec8d609555623cd2fb48d
Patrick Ambli <patrick.ambli@gmail.com>
2016-02-05 14:02:01 -0600

    Update socket.js

--- pvpAsteroidsV2/socket.js
+++ pvpAsteroidsV2/socket.js
@@ -1,1 +1,13 @@
-
+var url = 'ws://echo.websocket.org';
+var ws = new WebSocket(url);
+console.log('connecting to');
+ws.onopen = function () {
+  ws.send('Ping'); // Send the message 'Ping' to the server]
+  console.log('connected');
+};
+ws.onerror = function (error) {
+  console.error('WebSocket Error: ' + error);
+};
+ws.onmessage = function (e) {
+  console.log('Server: ' + e.data);
+};
