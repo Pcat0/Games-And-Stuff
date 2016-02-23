ba8f440cb904cb3d98d7b4051b4fc193161e8306
Patrick Ambli <patrick.ambli@gmail.com>
2016-02-23 15:09:28 -0600

    Create formatter.js

---
+++ Bookmark-Scripts/Formater/formatter.js
@@ -0,0 +1,17 @@
+function format(str){
+    var ex = ['⁰','²','³','⁴','⁵','⁶','⁷','⁸','⁹'];
+    (str.match(/\^([0-9]+)/g)||[]).forEach(function(a,b,c){
+        a = a.replace('\^', '');
+        ex.forEach(function(d,e){a = a.replace(new RegExp(e + 1, 'g'), d);});
+        str=str.replace(c[b],a);
+    });
+    (str.match(/root\([0-9]*\)/g)||[]).forEach(function(a,b,c){
+        a = a.slice(5,-1);
+        ex.forEach(function(d,e){a=a.replace(new RegExp(e + 1, 'g'), d);});
+        str=str.replace(c[b], a+'√');
+    });
+    str = str.replace(/\+\/-/g, '±');
+    str = str.replace(/sq/g, '√');
+    str = str.replace(/inf/g, '∞');
+    return str;
+}
