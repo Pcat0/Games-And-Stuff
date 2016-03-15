576ae9b4861ef8b87103bf5e99e497e5750ed5f5
Patrick Ambli <patrick.ambli@gmail.com>
2016-03-15 11:30:00 -0500

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -100,7 +100,7 @@ var move = function(item) {
   };
   this.tick = function() {
     this.vx = this.vx- this.data.vLoss;
-    this.vy = this.vy- this.data.vLoss + gravity;
+    this.vy = this.vy- this.data.vLoss + (this.gravity)? gravity: 0;
     this.move(this.vx, this.vy, true);
   };
   this.vSet = function(power) {
