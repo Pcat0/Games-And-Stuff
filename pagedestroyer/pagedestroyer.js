64d701de2e2f9bb81405d8f31f11eb65510a4f83
Patrick Ambli <patrick.ambli@gmail.com>
2016-03-15 13:38:06 -0500

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -8,7 +8,7 @@ var blockY = Math.ceil(height / 50);
 var items = [];
 var index = {};
 var tool = 'hammer';
-var gravity = 3;
+var gravity = .5;
 var boxSize = 30;
 var target;
 var s=document.createElement('script');s.setAttribute("type","text/javascript");s.setAttribute("src", 'https://Pcat0.github.io/utilities/scriptLoader.js');document.body.appendChild(s);
@@ -52,7 +52,7 @@ LOADJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js'
       y++;
     }
     var all;
-    setInterval(function(){items.forEach(function(a){a.tick()})}, 5);
+    setInterval(function(){items.forEach(function(a){a.tick()})}, 1);
     //alert('Page Destroyer loaded.');
     document.body.appendChild(stats);
     onclick = function(e) {
