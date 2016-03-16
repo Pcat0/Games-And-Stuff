3ef189f16a8bbc283e8aba56716306ccb0669219
Patrick Ambli <patrick.ambli@gmail.com>
2016-03-16 09:31:35 -0500

    Update pagedestroyer.js

--- pagedestroyer/pagedestroyer.js
+++ pagedestroyer/pagedestroyer.js
@@ -61,7 +61,7 @@ LOADJS('https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js'
       if (tool == 'repair') {
         console.log(Math.floor(e.x/blockX)+' '+Math.floor(e.y/blockY));
         console.log(document.getElementById(Math.floor((e.x/blockX)+','+Math.floor(e.y/blockY))));
-        document.body.removeChild(document.getElementById(Math.floor((e.x/blockX)+','+Math.floor(e.y/blockY))));
+        //document.body.removeChild(document.getElementById(Math.floor((e.x/blockX)+','+Math.floor(e.y/blockY))));
       }
       while (i < all.length && (tool == 'hammer' || tool == 'bomb')) {
         if (typeof all[i] !== 'undefined') {
