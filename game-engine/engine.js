beb3c6da093e48f3838e6302f809eda380779860
Patrick Ambli <patrick.ambli@gmail.com>
2016-03-31 09:39:03 -0500

    Create engine.js

---
+++ game-engine/engine.js
@@ -0,0 +1,18 @@
+Array.prototype.addArray = function(A){var out = [];for(var i=0; i< this.length; i++){out.push(this[i]+A[i])} return out;};
+Array.prototype.subtractArray = function(A){var out = [];for(var i=0; i< this.length; i++){out.push(this[i]-A[i])} return out;};
+var newCall = function(Cls,arg) {
+    return new (Function.prototype.bind.apply(Cls, [null].concat(arg)));
+};
+var Point = function(x,y,z){
+  this.tuple = [x,y,z];
+  this.addVectorToPoint=function(v){return (this.tuple = this.tuple.addArray(v.tuple));};
+  this.subtractVectorFromPoint=function(v){return (this.tuple = this.tuple.subtractArray(v.tuple));};
+  this.subtractPointFromPoint=function(p){return newCall(Vector, this.tuple.subtractArray(p.tuple))};
+  this.drawPoint=function(){console.log(this.tuple);};
+};
+var Vector = function(x,y,z){
+	this.tuple = [x,y,z];
+	this.AddVectorToVector = function(v){return newCall(Vector, this.tuple.addArray(v.tuple))};
+	this.SubtractVectorFromVector = function(v){return newCall(Vector, this.tuple.subtractArray(v.tuple))};
+	
+};
